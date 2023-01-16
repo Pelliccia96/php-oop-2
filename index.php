a16769dd74973d271a5a0126f6e32956b94a85c5
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-16 18:29:58 +0100

    Completed foreach structure

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ include_once "./classes/Category.php";
 include_once "./classes/Product.php";
 
 $productsList = [
-    $newCategory_1 = new Category("https://picsum.photos/100/100", "Pippo", "14.90", "dog", "cibo", "gioco", "cuccia"),
-    $newCategory_2 = new Category("https://picsum.photos/100/100", "Yuki", "9.90", "cat", "cibo", "gioco", "cuccia"),
-    $newCategory_3 = new Category("https://picsum.photos/100/100", "Jeppy", "6.50", "cat", "cibo", "gioco", "cuccia"),
-    $newCategory_4 = new Category("https://picsum.photos/100/100", "Sam", "19.90", "dog", "cibo", "gioco", "cuccia"),
-    $newCategory_5 = new Category("https://picsum.photos/100/100", "Bobby", "17.40", "dog", "cibo", "gioco", "cuccia"),
-    $newCategory_6 = new Category("https://picsum.photos/100/100", "Stella", "15.50", "cat", "cibo", "gioco", "cuccia"),
+    $newCategory_1 = new Category("https://picsum.photos/id/237/350/400", "Pippo", "14.90", "dog", "Carne in scatola", "Palla", "Casa"),
+    $newCategory_2 = new Category("https://picsum.photos/id/593/350/400", "Yuki", "9.90", "cat", "Crocchette", "Peluche", "Cartone"),
+    $newCategory_3 = new Category("https://picsum.photos/id/582/350/400", "Jeppy", "6.50", "cat", "Crocchette", "Gomitolo", "Cesto"),
+    $newCategory_4 = new Category("https://picsum.photos/id/200/350/400", "Sam", "19.90", "dog", "Carne in scatola", "Osso", "Casa"),
+    $newCategory_5 = new Category("https://picsum.photos/id/433/350/400", "Bobby", "17.40", "dog", "Carne in scatola", "Pupazzo", "Cesto"),
+    $newCategory_6 = new Category("https://picsum.photos/id/219/350/400", "Stella", "15.50", "cat", "Crocchette", "Filo", "Cartone"),
 ];
 
 /* var_dump($productsList); */
@@ -26,7 +26,7 @@ $productsList = [
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body class="bg-dark">
-    <div class="container text-white">
+    <div class="container text-white my-5">
         <div class="mt-3 mb-5">
             <h1 class="pt-3 text-center">PHP-OOP-2</h1>
         </div>
@@ -37,13 +37,13 @@ $productsList = [
                         <img src="<?php echo $product->getImage() ?>" class="card-img-top" alt="">
                         <div class="card-body text-black">
                             <h5 class="card-title"><?php echo $product->getTitle() ?></h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt vitae, distinctio dolorum accusamus nobis eligendi beatae.</p>
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">
                                 <div>
                                     <h6>Prezzo</h6>
-                                    <p><?php echo $product->getPrice() ?></p>
+                                    <p><?php echo $product->getPrice() ?> €</p>
                                 </div>
                             </li>
                             <li class="list-group-item">
